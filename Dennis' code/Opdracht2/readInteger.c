343ce4e6b77f77ab4a6e4e2ea1734c27be5c563a
Dennis de Jong <dennisdejong@student.tudelft.nl>
2016-04-05 15:53:13 +0200

    werkend

--- Dennis' code/Opdracht2/readInteger.c
+++ Dennis' code/Opdracht2/readInteger.c
@@ -16,34 +16,22 @@
 #define TxD PD1
 
 int readInteger(){
-    int i = 0;
-    int iVal = 16;
-    if(UDR0 != 0){
-        i = UDR0 - '0';
-        UDR0 = 0;
-    }
-    /*while(iVal != 10 || iVal != 13){
-        PORTB = 0xFF & iVal;
-        if(UDR0 != 0){
-            iVal = UDR0 - '0';
-            UDR0 = 0;
-        }
-
-        /*if(UDR0 != 0){
-            buffer = UDR0;
-            UDR0 = 0;
-            if(buffer >= '0' && buffer <= '9' ){
-                i = i*10 + (buffer-'0');
-            }
-        }
+    //int i = '0';
+    int value = 0;
+    /*while(i != 10 || i != 13){
+        value += (i-'0');
+        while(~UCSR0A & (1<<RXC0));
+        i = UDR0;
     }*/
-    return i;
+    while(~UCSR0A & (1<<RXC0));
+    value = UDR0 - '0';
+    return value;
 }
 
 int main(void) {
     
     UCSR0A |= (1 << U2X0);     //double usart snelheid i.v.m. nauwkwurigheid baudrate instelling.
-    UCSR0B = (1 << RXEN0);     // Enable  de USART Receiver
+    UCSR0B = (1 << TXEN0) |(1<<RXEN0);      // Enable  de USART Receiver
     UCSR0C = (1 << UCSZ01) | (1 << UCSZ00); /* 8 data bits, 1 stop bit */
     
     UBRR0H=00;
