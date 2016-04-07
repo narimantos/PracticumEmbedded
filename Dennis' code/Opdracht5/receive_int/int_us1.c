51200c5b28eefec5248f7ab588e18fa4763b167b
Dennis de Jong <dennisdejong@student.tudelft.nl>
2016-04-07 22:41:18 +0200

    hoi

--- Dennis' code/Opdracht5/receive_int/int_us1.c
+++ Dennis' code/Opdracht5/receive_int/int_us1.c
@@ -32,18 +32,9 @@ ISR(TIMER1_OVF_vect) {
     TIMSK1 &= ~(1<< TOIE0);
 }
 
-void vertraging(uint8_t w)
-{
-    uint16_t beginTijd = 0xFFFF;
-    beginTijd -= (w*1000000/256)
-    initTimer(beginTijd);
-}
-void initTimer(uint16_t beginTijd) {
+void initTimer() {
     TCCR1B |= (1 << CS12);
     TIMSK1 |= (1<< TOIE0);
-    TCNT1L = (beginTijd & 0xFF);
-    TCNT1H = (beginTijd>>8) & 0xFF);
-    
 }
 
 
@@ -69,94 +60,19 @@ int main(void) {
             case '0':
                 PORTB=0x01;
                 rx='2';
-                vertraging(10);
+                initTimer();
+                TCNT1 = 0xFFFF-(10*(1000000/256));
                 break;
             case '1':
                 PORTB=0x02;
                 rx='2';
-                vertraging(5);
+                initTimer();
+                TCNT1 = 0xFFFF-(5*(1000000/256));
                 break;
-            default : PORTB=0;
         }
         
         
     }  
     
     return (0);                           
-}
-// Quick Demo of light sensor
-
-// ------- Preamble -------- //
-#define F_CPU 1000000
-#include <avr/io.h>
-#include <util/delay.h>
-#include <stdlib.h>
-#include <avr/interrupt.h>
-
-#include "usart_fnct.h"
-
-// -------- Functions --------- //
-
-volatile char rx='2';
-
-void initUsart()
-{
-  UCSR0A |= (1 << U2X0); 
-  UCSR0B = (1 << TXEN0) |(1<<RXEN0) | (1<<RXCIE0);
-  UCSR0C = (1 << UCSZ01) | (1 << UCSZ00); 
-  UBRR0H=00;  UBRR0L=12; 
-}
-
-ISR(USART_RX_vect)
-{
-   rx=UDR0;
-}
-
-ISR(TIMER1_OVF_vect) {
-    PORTB |= (1<<PORTB0);
-    
-}
-
- void vertraging(uint8_t w)
- {
-    for(int i=0;i<w;i++)
-        _delay_ms(100);
-
- }
-
-int main(void) {
-
-    DDRB=0xFF;
-
-    initUsart();
-
-    sei();
-
-    writeString("Voer in:\n\r");
-    writeString("0, LED op B0 gaat 10 seconden aan.\n\r");
-    writeString("1, LED op B1 gaat 5 seconden aan.\n\r");
-    
-    TCCR1B |= 1 << CS01 | 0 << CS00;
-    TIMSK0 |= (1<< TOIE0);
-  
-  while (1) {
-
-
-    switch(rx) {
-
-    case '0': PORTB=0x01;
-              rx='2';
-              vertraging(100);
-              break;
-    case '1': PORTB=0x02;
-              rx='2';
-              vertraging(50);
-              break;
-    default : PORTB=0;
-    }
-    
-
-  }  
-
-  return (0);                           
 }
